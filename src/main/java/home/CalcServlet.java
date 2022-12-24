e8ab742170587af4a97a3a21843b9e2d0f2f444e
MahmudSaidxonov <saidxonovmaxmudjon@gmail.com>
2022-12-24 13:30:44 +0500

    Created calculator using Java Servlet html

---
+++ src/main/java/home/CalcServlet.java
@@ -0,0 +1,69 @@
+package home;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/Answer")
+
+public class CalcServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter pw = resp.getWriter();
+        int num1, num2;
+
+        pw.println("<center>");
+        pw.println("""
+                <style>
+                    body{background: palegreen}
+                </style>""");
+        pw.println("<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>\n");
+        String str = req.getParameter("operation");
+        num1 = Integer.parseInt(req.getParameter("first"));
+        num2 = Integer.parseInt(req.getParameter("second"));
+        switch (str) {
+            case "+" -> pw.println("<h1>Result: " + (num1 + num2) + "</h1>");
+            case "-" -> pw.println("<h1>Result: " + (num1 - num2) + "</h1>");
+            case "*" -> pw.println("<h1>Result: " + (num1 * num2) + "</h1>");
+            case "%" -> {
+                if (num1 % num2 != 0) {
+                    pw.println("<h1>Result: " + ((float)num1 / num2) + "</h1>");
+                }
+                else {
+                    pw.println("<h1>Result: " + (num1 / num2) + "</h1>");
+                }
+            }
+        }
+        pw.println("<a href=index.html>BACK</a>");
+        pw.println("</center>");
+    }
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.setContentType("text/html");
+//        PrintWriter pw = resp.getWriter();
+//        int num1, num2;
+//        String str = "";
+//        num1 = Integer.parseInt(req.getParameter("first"));
+//        num2 = Integer.parseInt(req.getParameter("second"));
+//        str = req.getParameter("operation");
+//
+//        if (str.equals("+")) {
+//            pw.println("<h1>Result: " + (num1 + num2) + "</h1>");
+//        }
+//        else if (str.equals("-")) {
+//            pw.println("Result: " + (num1 - num2));
+//        }
+//        else if (str.equals("*")) {
+//            pw.println("Result: " + (num1 * num2));
+//        }
+//        else {
+//            pw.println("Result: " + (num1 % num2));
+//        }
+//    }
+}
